3f90a56fcdedb91299ebde5e62c49875702c5577
이진(Lee Jin)(jin2) <jin2@ncsoft.com>
2020-09-29 13:15:42 +0900

    First commit

---
+++ src/build/precache-manifest.577c7a72fe266bc7a21e7c8376d01071.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c985d1a311b6d71a9f243de1da06d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd94a78e6da5ab6d1384",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "42dcac51a0a1225718b8",
+    "url": "/static/js/2.50dc2fc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.50dc2fc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd94a78e6da5ab6d1384",
+    "url": "/static/js/main.f4504ad9.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
